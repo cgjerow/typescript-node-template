e8207896eda9db74862ee6c8a52cf82a0bd51c42
connorjerow <cgjerow@gmail.com>
2024-11-18 11:24:29 -0800

    init containerized express app

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "./index.js";
-import { describe, it, expect } from "@jest/globals";
-
-describe("one", () => {
-  it("works", () => {
-    expect(test()).toEqual(true);
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-export const test = () => {
-    const x = 'test'
-    console.log(x)
-    return true
-}
+import express, { Express } from 'express'
+
+const app: Express = express()
+const port = process.env.PORT || '3000'
+
+app.get("/", (req, res) => {
+    res.send("Hello, World\n\n" + JSON.stringify(req.body));
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
